e89763470bb8749d191cc5312ae6ecc8965d86e6
Mojolaoluwa Gafar <Mojolaoluwa1212@gmail.com>
2026-06-20 11:23:09 +0100

    Cors ??

--- index.ts
+++ index.ts
@@ -10,24 +10,28 @@ dotenv.config()
 const app : Application = express()
 
 
-const allowedOrigins = new Set([
-    "https://mindora-client-two.vercel.app",
-    "http://localhost:5173",
-    "http://localhost:5174"
-])
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.has(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-app.use(express.json())
+// const allowedOrigins = new Set([
+//     "https://mindora-client-two.vercel.app",
+//     "http://localhost:5173",
+//     "http://localhost:5174"
+// ])
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.has(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+app.use(express.json());
+app.use(cors({
+  origin : "https://mindora-client-two.vercel.app",
+  credentials : true
+}));
 
 app.get("/", (req : Request , res : Response)=>{  
     res.status(200).json({ success : true, message : "Mindora Server running..."})
